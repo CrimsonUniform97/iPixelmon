5c17371da90ca704fe813dcda2269bb331591b44
colbymchenry <admin@colbymchenry.com>
2016-09-11 11:49:55 -0400

    Added getNames and getUUIDs to UUIDManager. Need to test this out.

--- src/main/java/ipixelmon/pixelbay/gui/buy/PacketBuyPokemon.java
+++ src/main/java/ipixelmon/pixelbay/gui/buy/PacketBuyPokemon.java
@@ -1,7 +1,14 @@
 package ipixelmon.pixelbay.gui.buy;
 
 import com.pixelmonmod.pixelmon.comm.PixelmonData;
+import com.pixelmonmod.pixelmon.config.PixelmonEntityList;
+import com.pixelmonmod.pixelmon.entities.pixelmon.EntityPixelmon;
+import com.pixelmonmod.pixelmon.enums.EnumPokemon;
+import com.pixelmonmod.pixelmon.storage.PixelmonStorage;
 import io.netty.buffer.ByteBuf;
+import ipixelmon.PixelmonUtility;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
@@ -9,23 +16,27 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 import java.util.UUID;
 
-public class PacketBuyPokemon implements IMessage {
+public class PacketBuyPokemon implements IMessage
+{
 
-    public PacketBuyPokemon() {
+    public PacketBuyPokemon()
+    {
     }
 
     private PixelmonData pixelmonData;
     private UUID seller;
     private int price;
 
-    public PacketBuyPokemon(PixelmonData pixelmonData, UUID seller, int price) {
+    public PacketBuyPokemon(PixelmonData pixelmonData, UUID seller, int price)
+    {
         this.pixelmonData = pixelmonData;
         this.seller = seller;
         this.price = price;
     }
 
     @Override
-    public void fromBytes(ByteBuf buf) {
+    public void fromBytes(ByteBuf buf)
+    {
         this.pixelmonData = new PixelmonData();
         pixelmonData.decodeInto(buf);
         seller = UUID.fromString(ByteBufUtils.readUTF8String(buf));
@@ -33,18 +44,53 @@ public class PacketBuyPokemon implements IMessage {
     }
 
     @Override
-    public void toBytes(ByteBuf buf) {
+    public void toBytes(ByteBuf buf)
+    {
         pixelmonData.encodeInto(buf);
         ByteBufUtils.writeUTF8String(buf, seller.toString());
         buf.writeInt(price);
     }
 
-    public static class Handler implements IMessageHandler<PacketBuyPokemon, IMessage> {
+    public static class Handler implements IMessageHandler<PacketBuyPokemon, IMessage>
+    {
 
         @Override
-        public IMessage onMessage(PacketBuyPokemon message, MessageContext ctx) {
+        public IMessage onMessage(PacketBuyPokemon message, MessageContext ctx)
+        {
             // TODO: Give money to buyer. Can use  final PlayerStorage targetStorage = PixelmonStorage.PokeballManager.getPlayerStorageFromUUIDOffline
             // TODO: Actually finish this
+
+            EntityPlayerMP player = ctx.getServerHandler().playerEntity;
+
+            if (player == null) return null;
+
+            try
+            {
+                if (message.pixelmonData == null)
+                {
+                    throw new Exception("ItemStack is null.");
+                }
+
+                if (message.seller == null)
+                {
+                    throw new Exception("Seller is null.");
+                }
+
+                if (message.price > PixelmonUtility.getServerBalance(player))
+                {
+                    throw new Exception("Insufficient PokéDollars.");
+                }
+
+                EntityPixelmon pokemon = (EntityPixelmon) PixelmonEntityList.createEntityByName(message.pixelmonData.name, player.worldObj);
+                pokemon.getLvl().setLevel(message.pixelmonData.lvl);
+                pokemon.setIsShiny(message.pixelmonData.isShiny);
+                pokemon.setHealth(message.pixelmonData.health);
+                PixelmonStorage.PokeballManager.getPlayerStorage(player).addToParty(pokemon);
+
+            } catch (Exception e)
+            {
+                player.addChatComponentMessage(new ChatComponentText(e.getMessage()));
+            }
             return null;
         }
     }
